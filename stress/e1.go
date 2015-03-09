98a2d1baf7448519de02db6e22b5829474e532af
8legd <git@8legd.com>
2015-03-09 23:42:36 +0000

    stress testing experiment 1

--- stress/e1.go
+++ stress/e1.go
@@ -1,24 +1,51 @@
 package main
 
 import (
+  "io/ioutil"
   "log"
+  "net"
+  "net/http"
+  "net/url"
   "os"
   "os/signal"
-  "time"
-  "syscall"
+  "strconv"
   "sync"
-  "net/http"
+  "sync/atomic"
+  "syscall"
+  "time"
   "github.com/8legd/go-tigertonic"
 )
 
+var (
+  requestCounter int64
+  startedExperiment time.Time
+  stoppedExperiment time.Time
+  closing bool
+  wg sync.WaitGroup
+)
 
+// Experiment 1 starts a HTTP server with a basic handler and simulates 10 seconds processing time
+// for each request so we can easily overload the server
 func main() {
 
-  // Start the HTTP server
+  closing = false
+
   var handler http.HandlerFunc
   handler = func(w http.ResponseWriter, r *http.Request) {
+    // Retrieve the counter from the request.
+    var counter string
+    counter = r.FormValue("counter")
+    //log.Println("Begin Processing Request",counter,"At",time.Now())
+    // Sanity checking
+    if counter == "" {
+      http.Error(w, "you must specify a counter for each request", http.StatusBadRequest)
+      log.Println("Completed Processing Request With Error","At",time.Now())
+      return
+    }
     time.Sleep(10 * time.Second)
-    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Write([]byte(counter))
+    //log.Println("Completed Processing Request",counter,"At",time.Now())
     return
   }
   server := tigertonic.NewServer(":8000", handler)
@@ -28,26 +55,47 @@ func main() {
   go func() {
     err := server.ListenAndServe()
     if nil != err {
-      log.Println(err)
+      if opError, ok := err.(*net.OpError); ok &&
+        (opError.Err.Error() == "use of closed network connection") &&
+        closing {
+        // This is acceptable if we are closing the server
+      } else {
+        log.Println("Unexpected Server Error",err)
+      }
     }
   }()
 
+  // Send 10000 simultaneous requests to overload the server (well does on my laptop!)
   var wg sync.WaitGroup
-  wg.Add(1000)
-  log.Println("Starting Experiment At",time.Now())
-  for i := 0; i<1000; i++ {
+  wg.Add(10000)
+  startedExperiment = time.Now()
+  log.Println("Starting Experiment At",startedExperiment)
+  for i := 0; i<10000; i++ {
     go func() {
       defer wg.Done()
-      res, err := http.Get("http://localhost:8000/")
+      atomic.AddInt64(&requestCounter, 1)
+      //log.Println("Sending Request",requestCounter,"At",time.Now())
+      res, err := http.PostForm("http://localhost:8000/",url.Values{"counter": {strconv.FormatInt(requestCounter,10)}})
       if err != nil {
+        log.Println("Error Sending Request",requestCounter,"At",time.Now(),err)
         log.Println(err)
       } else {
-        log.Println("Reading Response ",res.StatusCode)
+        defer res.Body.Close()
+        _, err := ioutil.ReadAll(res.Body)
+        if err != nil {
+          log.Println("Error Reading Response","At",time.Now(),err)
+          log.Println(err)
+        } else {
+          atomic.AddInt64(&requestCounter, -1)
+          //log.Println("Reading Response",string(body),"At",time.Now(),res.StatusCode)
+        }
       }
     }()
   }
   wg.Wait()
-  log.Println("Stopping Experiment At",time.Now())
+  stoppedExperiment = time.Now()
+  log.Println("Stopping Experiment At",stoppedExperiment,"Successfully Read",10000-requestCounter,"Requests In",stoppedExperiment.Sub(startedExperiment))
+
 
   // By creating an un-buffered chanel the main goroutine will in effect block here
   // waiting for an os.Signal at which point we can stop the server gracefully
@@ -55,8 +103,7 @@ func main() {
   signal.Notify(ch, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
   s := <-ch
   log.Println("Closing server on OS signal: ",s)
+  closing = true
   server.Close() // this closes all connections
-
-
 
 }
